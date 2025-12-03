ebc1df16075a95a8c5e34e39945422ab0d46e0c0
Lasse Henrich <lasse.finn.henrich@gmail.com>
2025-12-03 15:20:41 +0900

    epsilon tests

--- source/scheduler.py
+++ source/scheduler.py
@@ -8,8 +8,15 @@ class EpsilonDecay():
     epsilon_end: float
     decay_rate: float
 
-    def __call__(self, t):
+    def __call__(self, t): # todo: change this to be linear
+        factor: float
+        linear = False # otherwise exponential
+        if linear:
+            factor = max(0.0, 1.0 - t / self.decay_rate)
+        else:
+            factor = math.exp(-t / self.decay_rate)
+
         epsilon = self.epsilon_end + \
             (self.epsilon_start - self.epsilon_end) * \
-            math.exp(-t / self.decay_rate)
+            factor
         return epsilon
